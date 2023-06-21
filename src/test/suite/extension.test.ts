fec6411d142df6beb0c341c05833e615d8e46e05
Eshnek <dylanpozarnsky@gmail.com>
2023-06-21 15:26:21 -0600

    Passthrough

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,14 +1,18 @@
 import * as assert from 'assert';
 import * as util from '../../util';
-// import * as vscode from 'vscode';
+import * as vscode from 'vscode';
 
 import * as mocha from 'mocha';
 const describe = mocha.describe;
 
 suite('Extension Test Suite', () => {
+    const TEST_FOLDER = '/opt';
+
     describe('util', () => {
         test('util.system', async () => {
-            const result = await util.system("pwd");
+            (vscode.workspace.workspaceFolders as any) = [{url: {fsPath: TEST_FOLDER}}];
+
+            const result = await util.system('pwd');
 
             console.info(`pwd result is ${result}`);
 
